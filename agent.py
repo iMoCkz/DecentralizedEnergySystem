7afb1ce9255682d71ad3e8641757cea2036ca010
Maxe <bertram.max@web.de>
2020-05-25 10:58:48 +0200

    expanded step function and marketplace handles energy requests

--- agent.py
+++ agent.py
@@ -11,17 +11,16 @@ def _read_file(path):
 class Agent:
     def __init__(self, name, data):
         self.name = name
+        self._withdraw_factor = 0.8
         self._market = None
 
-        self._electrical_reserve_fund = 0
-        self._thermal_reserve_fund = 0
-
         self.electrical_consumption = 0
-        # self.electrical_production = 0
         self.electrical_storage = 0
         self.thermal_consumption = 0
-        # self.thermal_production = 0
         self.thermal_storage = 0
+
+        self._offered_electrical_power = 0
+        self._offered_thermal_power = 0
 
         self._electrical_consumption_data = None
         self._electrical_production_data = None
@@ -35,15 +34,6 @@ class Agent:
             self._get_data(key, val)
 
         # test = self._electrical_consumption_data[:5]
-
-    @property
-    def marketplace(self):
-        return self._market
-
-    @marketplace.setter
-    def marketplace(self, market):
-        self._market = market
-        self._market.register_participant(self)
 
     def _get_data(self, key, path):
         if key == 'electrical_c':
@@ -59,10 +49,42 @@ class Agent:
         elif key == 'thermal_s':
             self._thermal_storage_data = _read_file(path)
 
+    @property
+    def marketplace(self):
+        return self._market
+
+    @marketplace.setter
+    def marketplace(self, market):
+        self._market = market
+        self._market.register_participant(self)
+
+    @property
+    def offered_electrical_power(self):
+        return self._offered_electrical_power
+
+    def purchase_electrical_power(self, withdrawal):
+        if self._offered_electrical_power - withdrawal >= 0:
+            self._offered_electrical_power -= withdrawal
+            self.electrical_storage -= withdrawal
+        else:
+            print('Error while withdrawing electrical power.')
+
+    @property
+    def offered_thermal_power(self):
+        return self._offered_thermal_power
+
+    def purchase_thermal_power(self, withdrawal):
+        if self._offered_thermal_power - withdrawal >= 0:
+            self._offered_thermal_power -= withdrawal
+            self.thermal_storage -= withdrawal
+        else:
+            print('Error while withdrawing thermal power.')
+
     def step(self):
         # energy turnover
         if self._electrical_production_data is not None:
             self._electrical_storage += self._electrical_production_data.pop(0)
+            self._offered_electrical_power *= self._withdraw_factor
 
         if self._electrical_consumption_data is not None:
             consumption = self._electrical_consumption_data.pop(0)
@@ -76,7 +98,20 @@ class Agent:
                 self._market.request_electrical_energy(self, consumption)
 
         # thermal turnover
+        if self._thermal_production_data is not None:
+            self._thermal_storage += self._thermal_production_data.pop(0)
+            self._offered_thermal_power *= self._withdraw_factor
 
+        if self._thermal_consumption_data is not None:
+            consumption = self._thermal_consumption_data.pop(0)
+            if self.thermal_storage >= consumption:
+                self.thermal_storage -= consumption
+            else:
+                # deplete current thermal storage
+                consumption -= self.thermal_storage
+                self.thermal_storage = 0
+                # request on market
+                self._market.request_thermal_energy(self, consumption)
 
 
 
--- marketplace.py
+++ marketplace.py
@@ -8,8 +8,26 @@ class Marketplace:
             self.market_participants.append(agent)
             print('{0} has been registered to the marketplace {1}.'.format(agent.name, self.name))
 
-    def request_electrical_energy(self, agent, amount):
-        pass
+    def request_electrical_energy(self, requesting_agent, requested_amount):
+        print('{} requests {}kWh on the market.'.format(requesting_agent.name, requested_amount))
+        for agent in self.market_participants:
+            offered_el_power = agent.offered_electrical_power
+            # buy as much from an agent as possible
+            if agent is not requesting_agent and offered_el_power > 0:
+                if offered_el_power - requested_amount >= 0:
+                    agent.purchase_electrical_power(requested_amount)
+                    print('{} buys {}kWh from {}'.format(requesting_agent.name, requested_amount, agent.name))
+                else:
+                    requested_amount -= offered_el_power
+                    agent.offered_electrical_power = 0
+                    agent.purchase_electrical_power(offered_el_power)
+                    print('{} buys {}kWh from {}'.format(requesting_agent.name, offered_el_power, agent.name))
+            # if no more electrical power is needed, leave the market
+            if requested_amount == 0:
+                break
+        # if there is no more agent offering electrical power, requesting agent will buy from the market
+        if requested_amount > 0:
+            print('{} buys {}kWh from the grid operator'.format(requesting_agent.name, requested_amount))
 
-    def request_thermal_energy(self, agent, amount):
+    def request_thermal_energy(self, requesting_agent, amount):
         pass
